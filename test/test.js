3a0d32de3728dd189ac02bc24c1c0ade401295ea
Joshua Bartlett <jcbart12595@gmail.com>
2017-10-10 17:00:48 -0400

    adding circlci to the project

--- test/test.js
+++ test/test.js
@@ -1,9 +1,16 @@
 var assert = require('assert');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host     : 'db4free.net',
+  user     : 'aaaa_aaaa_aaaa',
+  password : 'aaaaaa',
+  database : 'aaaa_aaaa'
+});
 
 describe('Database', function() {
-  describe('#get()', function() {
-    it('should retrieve all of the items in the database', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+  describe('#connect()', function() {
+    it('should connect to the database without problems', function() {
+      return assert.equal(-1, [1,2,3].indexOf(4));
     });
   });
 });
